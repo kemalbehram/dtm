08a06b376615f66e5003ed5ae46d2fa3656dbcd6
Administrator <tianheng2017@qq.com>
2021-07-06 23:54:19 +0800

    change

--- app/admin/model/Commonpath.php
+++ app/admin/model/Commonpath.php
@@ -74,6 +74,8 @@ class Commonpath extends TimeModel
                     //查询末级叶子节点的上级在这一层的位置
                     $last_fid_position = array_search($last_fid, array_values($fid_level_child));
 
+                    var_dump($last_fid_position);exit;
+
                     //如果这个位置存在
                     if ($last_fid_position <> false) {
 
