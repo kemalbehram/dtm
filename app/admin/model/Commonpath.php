62cc0a36ae6870b6479ffa749293d70786583179
Administrator <tianheng2017@qq.com>
2021-07-07 00:28:38 +0800

    change

--- app/admin/model/Commonpath.php
+++ app/admin/model/Commonpath.php
@@ -74,6 +74,8 @@ class Commonpath extends TimeModel
                     $fid_level_child = self::where(['uid' => $fid, 'level' => $last_fid_level])->order('id','asc')->column('member_uid');
                     $fid_level_child = array_values($fid_level_child);
 
+                    var_dump($fid_level_child);
+
                     //查询末级叶子节点的上级在这一层的位置
                     $last_fid_position = array_search($last_fid, $fid_level_child);
 
